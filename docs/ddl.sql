b2d488e9cb2836c20d341b13d1449d37c2d70c69
creamsorry <noksm2@naver.com>
2024-10-17 20:35:47 +0900

    [docs] ddl - index 추가, erd - 연관관계 설명 추가

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -1,5 +1,7 @@
 -- FK 연관관계를 설정할 때, 개발자가 의도하지 않은 deadlock이나 cascade 관련 문제가 발생할 수 있습니다. 이러한 이유로 이번 프로젝트에서는 JPA 엔터티에서 연관관계를 직접 관리하고, DDL 작성 시 FK 제약 조건을 삭제하여 데이터베이스 제약 대신 JPA의 연관관계 관리 기능을 활용할 계획입니다.
 
+-- index 사용: 인덱스는 기존 외래 키(foreign key)와 상태 코드와 같이 조회가 자주 발생하는 컬럼에 적용했습니다. 이를 통해 조회 쿼리의 성능 향상을 기대할 수 있습니다.
+
 CREATE TABLE member (
     member_id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '사용자 ID',
     member_name VARCHAR(13) NOT NULL COMMENT '사용자 명',
@@ -12,7 +14,9 @@ CREATE TABLE waiting (
     token VARCHAR(255) NOT NULL COMMENT '토큰 값',
     status VARCHAR(10) NOT NULL COMMENT '상태 값',
     create_at DATETIME NOT NULL COMMENT '생성시간',
-    expired_at DATETIME COMMENT '만료시간'
+    expired_at DATETIME COMMENT '만료시간',
+    INDEX idx_status (status),        
+    INDEX idx_waiting_id (waiting_id)
 ) COMMENT = '대기열';
 
 CREATE TABLE concert (
@@ -27,7 +31,8 @@ CREATE TABLE concert_schedule (
     open_date DATETIME NOT NULL COMMENT '콘서트 개최 일',
     start_date DATETIME NOT NULL COMMENT '티켓 예매 시작 시간',
     end_date DATETIME NOT NULL COMMENT '티켓 예매 종료 시간',
-    capacity INT NOT NULL COMMENT '남은 좌석 수'
+    capacity INT NOT NULL COMMENT '남은 좌석 수',  
+    INDEX idx_concert_id (concert_id)
 ) COMMENT = '콘서트 일정';
 
 CREATE TABLE concert_seat (
@@ -35,7 +40,9 @@ CREATE TABLE concert_seat (
     schedule_id BIGINT NOT NULL,
     seat_num VARCHAR(3) NOT NULL COMMENT '좌석 번호',
     amount INT NOT NULL COMMENT '좌석 금액',
-    seat_status VARCHAR(10) NOT NULL COMMENT '좌석 점유 여부'
+    seat_status VARCHAR(10) NOT NULL COMMENT '좌석 점유 여부',  
+    INDEX idx_schedule_id (schedule_id),  
+    INDEX idx_seat_status (seat_status)
 ) COMMENT = '콘서트 좌석';
 
 CREATE TABLE reservation (
@@ -46,9 +53,11 @@ CREATE TABLE reservation (
     open_date DATETIME NOT NULL COMMENT '콘서트 개최 일',
     seat_num VARCHAR(3) NOT NULL COMMENT '좌석 번호',
     amount INT NOT NULL COMMENT '좌석 금액',
-    reserve_status VARCHAR(10) NOT NULL COMMENT '예약 상태'
+    reserve_status VARCHAR(10) NOT NULL COMMENT '예약 상태',  
+    INDEX idx_member_id (member_id),  
+    INDEX idx_reserve_status (reserve_status),
+    INDEX idx_seat_id (seat_id)
 ) COMMENT = '예약 정보';
-
 
 CREATE TABLE payment (
     pay_id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -56,7 +65,10 @@ CREATE TABLE payment (
     reserve_id BIGINT NOT NULL,
     amount INT NOT NULL COMMENT '결제 금액',
     is_pay BOOLEAN DEFAULT FALSE COMMENT '결제 여부(true / false)',
-    create_at DATETIME NOT NULL COMMENT '생성 시간'
+    create_at DATETIME NOT NULL COMMENT '생성 시간',
+    INDEX idx_member_id (member_id),
+    INDEX idx_reserve_id (reserve_id),
+    INDEX idx_is_pay (is_pay)
 ) COMMENT = '결제 정보';
 
 CREATE TABLE amount_history (
@@ -64,5 +76,7 @@ CREATE TABLE amount_history (
     member_id BIGINT NOT NULL,
     amount INT NOT NULL COMMENT '결제 금액',
     point_type VARCHAR(10) NOT NULL COMMENT '결제 타입 (CHARGE, USE)',
-    create_at DATETIME NOT NULL COMMENT '생성 일'
+    create_at DATETIME NOT NULL COMMENT '생성 일',
+    INDEX idx_member_id (member_id),
+    INDEX idx_point_type (point_type)
 ) COMMENT = '금액 이력';
